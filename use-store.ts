1668ae3a68c462b840b4318c584c315358b22f4f
Steven Beshensky <s.beshensky@gmail.com>
2022-12-04 18:10:13 -0600

    cleanup dependencies and uuid

--- use-store.ts
+++ use-store.ts
@@ -32,7 +32,7 @@ export function useWorkerSelector<
   });
 
   useDeepCompareEffect(() => {
-    const uuid = uuidv4();
+    const uuid = crypto.randomUUID();
     currentUuid.value = uuid;
 
     // remove the root state passed in once
@@ -51,13 +51,4 @@ export function useWorkerSelector<
   }, [selector, params]);
 
   return state;
-}
-
-function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  );
 }
